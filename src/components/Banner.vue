f08f0fa6c15485fa2063066fe25bdd8f10ba2786
Elias notebook <eliasbragga44@gmail.com>
2022-06-03 00:00:11 -0300

    First commit

---
+++ src/components/Banner.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id="main-banner">
+        <h1>Make Your Burger</h1>
+
+    </div>
+
+
+</template>
+
+
+<script>
+    export default {
+        name:"Banner"
+    }
+</script>
+
+<style scoped>
+#main-banner {
+    background-image: url('/public/img/burger.jpg');
+    background-position: 0 -250px;
+    background-size: cover;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    height: 500px;
+
+}
+
+#main-banner h1{
+    color: #FFF;
+    text-align: center;
+    font-size: 55px;
+    background-color: #222;
+    padding: 20px 40px;
+}
+
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,30 @@
+<template>
+    <footer id="footer">
+        <p>Elias-Braga &copy; 2021</p>
+
+    </footer>
+</template>
+
+<script>
+    export default {
+        name:"Footer"
+    }
+</script>
+
+<style scoped>
+#footer {
+    height: 200px;
+    background-color: #222;
+    border-top: 4px solid#111;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    bottom: 0;
+    width: 100%;
+    
+    color: #FCBA03;
+    background-repeat: no-repeat;
+}
+
+
+</style>
